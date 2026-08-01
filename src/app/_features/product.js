6e61f7108f81af0c449967b12a3cf24cc26b41dc
Tuguldur Tuvshintugs <146443087+tuguldur0@users.noreply.github.com>
2026-08-01 10:35:48 +0800

    jjkm

--- src/app/_features/product.js
+++ src/app/_features/product.js
@@ -1,11 +1,18 @@
 export function Product(props) {
-const {name, price, category, imageUrl} = props
-    return (
+const {name, price, category, imageUrl, stock, setStock} = props;
+
+const addToCart = () => {
+    setStock(stock - 1);
+}
+
+return (
     <div id="product">
         <div id="text">
-            <h1>{name}</h1>
-            <h1>{price}$</h1>
-            <h1>{category}</h1>
+            <h1>Product: {name}</h1>
+            <h1>Price: {price}$</h1>
+            <h1>Category: {category}</h1>
+            <h1>Stock: {stock}</h1>
+            <button id="button" onClick={addToCart}>Add to cart</button>
         </div>
         <img src={imageUrl} id="image"></img>
     </div>
--- src/app/page.js
+++ src/app/page.js
@@ -8,23 +8,27 @@ export default function Home() {
   const [price, setPrice] = useState("");
   const [category, setCategory] = useState("");
   const [imageUrl, setimageUrl] = useState("")
+  const [stock, setStock] = useState("")
 
   const Add = () => {
     if(name == ""){
       return;
-    } else if(price == ""){
+    } else if(price == "" || price <= 0){
       return;
     } else if(category == ""){
       return;
     } else if(imageUrl == ""){
       return;
+    } else if(stock == "" || stock <= 0){
+      return
     }
-    const newProducts = [...products, {id: products.length + 1, name: name, price: price, category: category, imageUrl: imageUrl}];
+    const newProducts = [...products, {id: products.length + 1, name: name, price: price, category: category, imageUrl: imageUrl, stock: stock}];
     setProducts(newProducts);
     setName("");
     setPrice("");
     setCategory("");
     setimageUrl("");
+    setStock("")
   }
   return (
     <div id="outterdiv">
@@ -34,21 +38,22 @@ export default function Home() {
           <div id="inputs">
             <input onChange={(event) => { setName(event.target.value) }} type="text" className="input"  value={name} placeholder="Name"></input>
             <input onChange={(event) => { setPrice(event.target.value) }} type="number" className="input" value={price} placeholder="Price"></input>
-            <label htmlFor="options">Category:</label>
-            <select id="options" value={category} onChange={(event) => setCategory(event.target.value)}>
-            <label value="" disabled>--Select--</label>
-              <option value="laptop">Laptop</option>
-              <option value="phone">Phone</option>
-              <option value="console">Console</option>
+            <label htmlFor="options"></label>
+            <select id="options"  className="input" value={category} onChange={(event) => setCategory(event.target.value)}>
+            <option value="" disabled>Category</option>
+              <option value="Laptop">Laptop</option>
+              <option value="Phone">Phone</option>
+              <option value="Console">Console</option>
             </select>
             <input onChange={(event) => { setimageUrl(event.target.value) }} type="text" className="input" value={imageUrl} placeholder="imageUrl"></input>
+            <input onChange={(event) => { setStock(event.target.value) }} type="number" className="input" value={stock} placeholder="Stock"></input>
           </div>
           <button onClick={Add} id="button">Add</button>
         </div>
         <h1 id="products-title">Products</h1>
         <div id="products">
           {products.map((product, index) => {
-            return <Product key = {index} name = {product.name} price = {product.price} category = {product.category} imageUrl = {product.imageUrl}/>
+            return <Product key = {index} name = {product.name} price = {product.price} category = {product.category} imageUrl = {product.imageUrl} stock = {product.stock} setStock = {setStock}/>
           })}
         </div>
       </div>
